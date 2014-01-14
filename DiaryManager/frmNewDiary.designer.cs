eb9aa70eac354e847b01b528ddb90a6d4a37c82a
zsf222 <zhushuofei@126.com>
2014-01-15 00:53:35 +0800

    ...

---
+++ DiaryManager/frmNewDiary.designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DiaryManager
+{
+    partial class creatNew
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weatherBox = new System.Windows.Forms.TextBox();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AcceptButton = new System.Windows.Forms.Button();
+            this.cancleButton = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // weatherBox
+            // 
+            this.weatherBox.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.weatherBox.Location = new System.Drawing.Point(16, 117);
+            this.weatherBox.Name = "weatherBox";
+            this.weatherBox.Size = new System.Drawing.Size(204, 30);
+            this.weatherBox.TabIndex = 2;
+            this.weatherBox.Text = "晴";
+            // 
+            // titleBox
+            // 
+            this.titleBox.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.titleBox.Location = new System.Drawing.Point(16, 200);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(204, 30);
+            this.titleBox.TabIndex = 3;
+            this.titleBox.Text = "这是一篇日记";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "天气";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "日期";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "标题";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // AcceptButton
+            // 
+            this.AcceptButton.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.AcceptButton.Location = new System.Drawing.Point(25, 255);
+            this.AcceptButton.Name = "AcceptButton";
+            this.AcceptButton.Size = new System.Drawing.Size(68, 35);
+            this.AcceptButton.TabIndex = 4;
+            this.AcceptButton.Text = "确认";
+            this.AcceptButton.UseVisualStyleBackColor = true;
+            this.AcceptButton.Click += new System.EventHandler(this.AcceptButton_Click);
+            // 
+            // cancleButton
+            // 
+            this.cancleButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancleButton.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cancleButton.Location = new System.Drawing.Point(138, 255);
+            this.cancleButton.Name = "cancleButton";
+            this.cancleButton.Size = new System.Drawing.Size(63, 35);
+            this.cancleButton.TabIndex = 5;
+            this.cancleButton.Text = "取消";
+            this.cancleButton.UseVisualStyleBackColor = true;
+            this.cancleButton.Click += new System.EventHandler(this.cancleButton_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 48);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(204, 30);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // creatNew
+            // 
+            this.AcceptButton = this.AcceptButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancleButton;
+            this.ClientSize = new System.Drawing.Size(234, 301);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cancleButton);
+            this.Controls.Add(this.AcceptButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.weatherBox);
+            this.Name = "creatNew";
+            this.Text = "新建";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button AcceptButton;
+        private System.Windows.Forms.Button cancleButton;
+        private System.Windows.Forms.TextBox weatherBox;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
